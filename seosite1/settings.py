e6d3bdf6234565f41ab4d81b45633e4d5ddf6ea4
NitishKaushal <70462326+NitishKaushal@users.noreply.github.com>
2020-10-21 08:59:46 +0530

    Update settings.py

--- seosite1/settings.py
+++ seosite1/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7b&1mpm!ixc@_s44g$@-xrp^&33^xq#!s8kwh#=t0(dwf*7xwa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
